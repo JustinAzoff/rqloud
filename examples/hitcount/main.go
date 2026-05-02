f3f635a0fa0f2faaaf31db6f4e4214565975c1c7
Justin Azoff <justin@azoff.dev>
2026-05-02 13:53:31 -0400

    improve the hitcount example

--- examples/hitcount/main.go
+++ examples/hitcount/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,9 +10,20 @@ import (
 )
 
 func main() {
-	// Use "hitcount" for a single node, or "hitcount-1", "hitcount-2", etc. for a cluster.
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: hitcount [hostname]\n\n")
+		fmt.Fprintf(flag.CommandLine.Output(), "Use \"hitcount\" for a single node, or \"hitcount-1\", \"hitcount-2\", etc. for a cluster.\n\n")
+		flag.PrintDefaults()
+	}
+	flag.Parse()
+
+	hostname := "hitcount"
+	if flag.NArg() > 0 {
+		hostname = flag.Arg(0)
+	}
+
 	srv := &rqloud.Server{
-		Hostname: "hitcount",
+		Hostname: hostname,
 	}
 	if err := srv.Start(); err != nil {
 		log.Fatal(err)
@@ -23,6 +35,7 @@ func main() {
 	db.Exec(`INSERT INTO hits (count) SELECT 0 WHERE NOT EXISTS (SELECT 1 FROM hits)`)
 
 	ln, _ := srv.Listen("tcp", ":80")
+	log.Printf("listening on http://%s/", hostname)
 	http.Serve(ln, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		db.Exec(`UPDATE hits SET count = count + 1`)
 		var count int
